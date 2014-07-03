df93ae316a441a50522e285738d50d96c8b90d0c
Caridy Patino <caridy@gmail.com>
2014-07-02 20:27:18 -0400

    first draft

--- README.md
+++ README.md
@@ -1,4 +1,146 @@
 es6-module-transpiler-amd-formatter
 ===================================
 
-ES6 Module Transpiler Extension to Output AMD `define()` Format 
+ES6 Module Transpiler Formatter to Output AMD `define()` Format
+
+## Overview
+
+ES6 Module Transpiler `es6-module-transpiler` is an experimental compiler that allows you to write your JavaScript using a subset of the current ES6 module syntax, and compile it into various formats. The `es6-module-transpiler-amd-formatter` is one of those output formats that is focus on enabling the use of ES6 modules thru [RequireJS][] today.
+
+[RequireJS]: http://www.requirejs.org/
+[es6-module-transpiler]: https://github.com/square/es6-module-transpiler
+
+## Disclaimer
+
+This output format compromises in few of the ES6 features in ES6 modules, including live bindings, sealed objects, etc. This compromise is Ok when you try to use them as AMD modules.
+
+## Usage
+
+### Build tools
+
+Since this formatters is an plugin for [es6-module-transpiler], you can use it with any existing build tool that supports [es6-module-transpiler] as the underlaying engine to transpile the ES6 modules. There several plugins developed for different build tools:
+
+* **Grunt:** [grunt-es6-module-transpiler](https://github.com/joefiorini/grunt-es6-module-transpiler), maintained by @joefiorini (not yet compatible with v0.5.x)
+* **Gulp:** [gulp-es6-module-transpiler](https://github.com/ryanseddon/gulp-es6-module-transpiler), maintained by @ryanseddon (not yet compatible with v0.5.x)
+* **Brunch:** [es6-module-transpiler-brunch](https://github.com/gcollazo/es6-module-transpiler-brunch), maintained by @gcollazo *(CommonJS only)* (not yet compatible with v0.5.x)
+* **Broccoli:** [broccoli-es6-concatenator](https://github.com/joliss/broccoli-es6-concatenator), maintained by @joliss (not yet compatible with v0.5.x)
+* **Mimosa:** [mimosa-es6-module-transpiler](https://github.com/dbashford/mimosa-es6-module-transpiler), maintained by @dbashford (not yet compatible with v0.5.x)
+
+You just need to make sure that `es6-module-transpiler-amd-formatter` is accessible for those tools, and pass the proper `formatter` option thru the [es6-module-transpiler][] configuration.
+
+### Executable
+
+If you plan to use the `compile-module` CLI, the formatters can be used directly from the command line:
+
+```
+$ npm install es6-module-transpiler
+$ npm install es6-module-transpiler-amd-formatter
+$ ./node_modules/.bin/compile-modules convert -f es6-module-transpiler-amd-formatter path/to/**/*.js -o build/
+```
+
+__The `-f` option allow you to specify the path to the specific formatter, which is this case is an installed module called `es6-module-transpiler-amd-formatter`.__
+
+### Library
+
+You can also use the formatter with the transpiler as a library:
+
+```javascript
+var transpiler = require('es6-module-transpiler');
+var AMDFormatter = require('es6-module-transpiler-amd-formatter');
+var Container = transpiler.Container;
+var FileResolver = transpiler.FileResolver;
+
+var container = new Container({
+  resolvers: [new FileResolver(['lib/'])],
+  formatter: new AMDFormatter()
+});
+
+container.getModule('index');
+container.write('out/mylib.js');
+```
+
+## Supported ES6 Module Syntax
+
+Again, this syntax is in flux and is closely tracking the module work being done by TC39. This package relies on the syntax supported by [es6-module-transpiler], which relies on [esprima], you can have more information about the supported syntax here: https://github.com/square/es6-module-transpiler#supported-es6-module-syntax
+
+[esprima]: https://github.com/ariya/esprima
+
+## Compiled Output
+
+First of all, the output format for `define()` might looks alien even for many developers, but considering that [es6-module-transpiler] relies on [Recast] to mutate the original ES6 code, it can output the corresponding [sourceMap], you should be able to debug the module code without having to understand the actual output format.
+
+[sourceMap]: http://www.html5rocks.com/en/tutorials/developertools/sourcemaps/
+[Recast]: https://github.com/benjamn/recast
+
+### Default export
+
+For a module without imports, and a single default exports:
+
+```javascript
+export default function (a, b) {
+  return a + b;
+}
+```
+
+will produce something like this:
+
+```javascript
+define("component/foo", ["exports"], function(__exports__) {
+  "use strict";
+
+  function __es6_export__(name, value) {
+    __exports__[name] = value;
+  }
+
+  __es6_export__("default", function(a, b) {
+    return a + b;
+  });
+});
+```
+
+### Imports and exports
+
+A more complex example will look like this:
+
+```javascript
+import assert from "./assert";
+
+export default function (a, b) {
+  assert(a);
+  assert(b);
+  return a + b;
+};
+```
+
+and the output will be:
+
+```javascript
+define("component/foo", ["./assert", "exports"], function(component$assert$$, __exports__) {
+  "use strict";
+
+  function __es6_export__(name, value) {
+    __exports__[name] = value;
+  }
+
+  var assert = component$assert$$["default"];
+  __es6_export__("assert", component$assert$$["assert"]);
+
+  __es6_export__("default", function(a, b) {
+    assert(a);
+    assert(b);
+    return a + b;
+  });
+});
+```
+
+Part of the goal, is try to preserve as much as possible the original code of the module within the factory function. Obviously, this is difficult when you have to export default functions and other declarations. The only modifications you will see in the body are the calls to the `__es6_export__()` method to export the new value when defined or updated, the rest of the code will remain immutable.
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+Thanks, and enjoy living in the ES6 future!
---
+++ index.js
@@ -0,0 +1,367 @@
+/* jshint node:true, undef:true, unused:true */
+
+var assert = require('assert');
+
+var recast = require('recast');
+var types = recast.types;
+var n = types.namedTypes;
+var b = types.builders;
+var Replacement = require('./lib/replacement');
+
+function sourcePosition(mod, node) {
+  var loc = node && node.loc;
+  if (loc) {
+    return mod.relativePath + ':' + loc.start.line + ':' + (loc.start.column + 1);
+  } else {
+    return mod.relativePath;
+  }
+}
+
+/**
+ * The 'YUI.add' setting for referencing exports aims to produce code that can
+ * be used in environments using YUI.
+ *
+ * @constructor
+ */
+function AMDFormatter() {}
+
+/**
+ * Returns an expression which globally references the export named by
+ * `identifier` for the given module `mod`. For example:
+ *
+ *    // rsvp/defer.js, export default
+ *    rsvp$defer$$.default
+ *
+ *    // rsvp/utils.js, export function isFunction
+ *    rsvp$utils$$.isFunction
+ *
+ * @param {Module} mod
+ * @param {ast-types.Identifier} identifier
+ * @return {ast-types.MemberExpression}
+ */
+AMDFormatter.prototype.reference = function(mod, identifier) {
+  return b.memberExpression(
+    b.identifier(mod.id),
+    n.Identifier.check(identifier) ? identifier : b.identifier(identifier),
+    false
+  );
+};
+
+/**
+ * Process a variable declaration found at the top level of the module. Since
+ * we do not need to rewrite exported variables, we can leave variable
+ * declarations alone.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} nodePath
+ * @return {?Array.<ast-types.Node>}
+ */
+AMDFormatter.prototype.processVariableDeclaration = function(/* mod, nodePath */) {
+  return null;
+};
+
+/**
+ * Process a variable declaration found at the top level of the module. Since
+ * we do not need to rewrite exported functions, we can leave function
+ * declarations alone.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} nodePath
+ * @returns {Array.<ast-types.Node>}
+ */
+AMDFormatter.prototype.processFunctionDeclaration = function(mod, nodePath) {
+  return null;
+};
+
+/**
+ * Because exported references are captured via a closure as part of a getter
+ * on the `exports` object, there's no need to rewrite local references to
+ * exported values. For example, `value` in this example can stay as is:
+ *
+ *   // a.js
+ *   export var value = 1;
+ *
+ * Would be rewritten to look something like this:
+ *
+ *   var value = 1;
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} referencePath
+ * @return {ast-types.Expression}
+ */
+AMDFormatter.prototype.exportedReference = function(mod, referencePath) {
+  return null;
+};
+
+/**
+ * Gets a reference to an imported binding by getting the value from the
+ * required module on demand. For example, this module:
+ *
+ *   // b.js
+ *   import { value } from './a';
+ *   console.log(value);
+ *
+ * Would be rewritten to look something like this:
+ *
+ *   var a$$ = require('./a');
+ *   console.log(a$$.value):
+ *
+ * If the given reference does not refer to an imported binding then no
+ * rewriting is required and `null` will be returned.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} referencePath
+ * @return {?ast-types.Expression}
+ */
+AMDFormatter.prototype.importedReference = function(mod, referencePath) {
+  return null;
+};
+
+/**
+ * We do not need to rewrite references to local declarations.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} referencePath
+ * @returns {?ast-types.Node}
+ */
+AMDFormatter.prototype.localReference = function(mod, referencePath) {
+  return null;
+};
+
+/**
+ * @param {Module} mod
+ * @param {ast-types.Expression} declaration
+ * @return {ast-types.Statement}
+ */
+AMDFormatter.prototype.defaultExport = function(mod, declaration) {
+  if (n.FunctionExpression.check(declaration)) {
+    // export default function <name> () {}
+    if (!declaration.id) {
+      // anonymous functionDeclaration
+      return [b.expressionStatement(
+        b.callExpression(b.identifier('__es6_export__'), [b.literal("default"), b.functionExpression(null, declaration.params, declaration.body)])
+      )];
+    } else {
+      // named functionDeclaration
+      return [
+        b.functionDeclaration(declaration.id, declaration.params, declaration.body),
+        b.expressionStatement(
+          b.callExpression(b.identifier('__es6_export__'), [b.literal("default"), declaration.id])
+        )
+      ];
+    }
+  } else if (n.VariableDeclaration.check(declaration)) {
+    // export default var foo = 1, bar = 2;
+    return [
+      b.variableDeclaration('var', declaration.declarations),
+      b.expressionStatement(
+        b.callExpression(b.identifier('__es6_export__'), [b.literal("default"), declaration.declarations[0].id])
+      )
+    ];
+  } else {
+    // export default {foo: 1};
+    return [b.expressionStatement(
+      b.callExpression(b.identifier('__es6_export__'), [b.literal("default"), declaration])
+    )];
+  }
+};
+
+/**
+ * Replaces non-default exports. For declarations we simply remove the `export`
+ * keyword. For export declarations that just specify bindings, e.g.
+ *
+ *   export { a, b };
+ *
+ * we remove them entirely since they'll be handled when we define properties on
+ * the `exports` object.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} nodePath
+ * @return {?Replacement}
+ */
+AMDFormatter.prototype.processExportDeclaration = function(mod, nodePath) {
+  var node = nodePath.node,
+    declaration = node.declaration;
+
+  if (n.FunctionDeclaration.check(declaration)) {
+    // export function <name> () {}
+    return Replacement.swaps(nodePath, [declaration, b.expressionStatement(
+      b.callExpression(b.identifier('__es6_export__'), [b.literal(declaration.id.name), declaration.id])
+    )]);
+  } else if (n.VariableDeclaration.check(declaration)) {
+    // export default var foo = 1;
+    return Replacement.swaps(nodePath, [declaration].concat(declaration.declarations.map(function (declaration) {
+      return b.expressionStatement(
+        b.callExpression(b.identifier('__es6_export__'), [b.literal(declaration.id.name), declaration.id])
+      );
+    })));
+  } else if (declaration) {
+    throw new Error('unexpected export style, found a declaration of type: ' + declaration.type);
+  } else {
+    return Replacement.removes(nodePath);
+  }
+};
+
+/**
+ * Since import declarations only control how we rewrite references we can just
+ * remove them -- they don't turn into any actual statements.
+ *
+ * @param {Module} mod
+ * @param {ast-types.NodePath} nodePath
+ * @return {?Replacement}
+ */
+AMDFormatter.prototype.processImportDeclaration = function(mod, nodePath) {
+  return Replacement.removes(nodePath);
+};
+
+/**
+ * Convert a list of ordered modules into a list of files.
+ *
+ * @param {Array.<Module>} modules Modules in execution order.
+ * @return {Array.<ast-types.File}
+ */
+AMDFormatter.prototype.build = function(modules) {
+  var self = this;
+  return modules.map(function(mod) {
+    var body = mod.ast.program.body,
+      meta = self.buildDependenciesMeta(mod);
+
+    // setting up all named imports, and re-exporting from as well
+    body.unshift.apply(body, self.buildPrelude(mod));
+
+    body.unshift(
+      // module body runs in strict mode.
+      b.expressionStatement(b.literal('use strict')),
+      // function __es6_export__ (name, value) provides a way to set named exports into __exports__
+      b.functionDeclaration(b.identifier('__es6_export__'), [b.identifier('name'), b.identifier('value')], b.blockStatement([
+        b.expressionStatement(b.assignmentExpression("=",
+          b.memberExpression(
+            b.identifier('__exports__'),
+            b.identifier('name'),
+            true
+          ),
+          b.identifier('value')
+        ))
+      ]))
+    );
+
+    // wrapping the body of the program with a define() call
+    mod.ast.program.body = [b.expressionStatement(b.callExpression(b.identifier('define'), [
+        // module name argument
+        b.literal(mod.name),
+        // depedencies
+        b.arrayExpression(meta.deps.concat(b.literal('exports'))),
+        // factory function
+        b.functionExpression(null, meta.identifiers.concat(b.identifier('__exports__')), b.blockStatement(body))
+    ]))];
+
+    mod.ast.filename = mod.relativePath;
+    return mod.ast;
+  });
+};
+
+/**
+ * Build a series of identifiers based on the imports (and exports with sources)
+ * in the given module.
+ *
+ * @private
+ * @param {Module} mod
+ * @return {
+ *   setters: {ast-types.Array}
+ *   deps: {ast-types.Array}
+ * }
+ */
+AMDFormatter.prototype.buildDependenciesMeta = function(mod) {
+  var requiredModules = [];
+  var importedModules = [];
+  var importedModuleIdentifiers = [];
+
+  // `(import|export) { ... } from 'math'`
+  [mod.imports, mod.exports].forEach(function(declarations) {
+    declarations.modules.forEach(function(sourceModule) {
+      if (~requiredModules.indexOf(sourceModule)) {
+        return;
+      }
+      requiredModules.push(sourceModule);
+      importedModuleIdentifiers.push(b.identifier(sourceModule.id));
+
+      var matchingDeclaration;
+      declarations.declarations.some(function(declaration) {
+        if (declaration.source === sourceModule) {
+          matchingDeclaration = declaration;
+          return true;
+        }
+      });
+
+      assert.ok(
+        matchingDeclaration,
+        'no matching declaration for source module: ' + sourceModule.relativePath
+      );
+
+      importedModules.push(b.literal(matchingDeclaration.sourcePath));
+    });
+  });
+
+  return {
+    identifiers: importedModuleIdentifiers, // [path$to$foo$$, path$to$bar$$]
+    deps: importedModules                   // ["./foo", "./bar"]
+  };
+};
+
+/**
+ * Set the scoped values for every named import (and exports from)
+ * in the given module.
+ *
+ * @private
+ * @param {Module} mod
+ * @return {Array[ast-types.Statement]}
+ */
+AMDFormatter.prototype.buildPrelude = function(mod) {
+  var prelude = [];
+
+  // import {foo} from "foo"; should hoist variables declaration
+  mod.imports.names.forEach(function (name) {
+    var importDeclaration = mod.imports.findSpecifierByName(name),
+      id = mod.getModule(importDeclaration.declaration.node.source.value).id;
+
+    prelude.push(b.variableDeclaration('var', [b.identifier(importDeclaration.name)]));
+    prelude.push(b.expressionStatement(b.assignmentExpression("=",
+      b.identifier(importDeclaration.name),
+      b.memberExpression(
+        b.identifier(id),
+        b.literal(importDeclaration.from),
+        true
+      )
+    )));
+  });
+
+  mod.exports.names.forEach(function(name) {
+    var specifier = mod.exports.findSpecifierByName(name),
+      id;
+
+    assert.ok(
+      specifier,
+      'no export specifier found for export name `' +
+      name + '` from ' + mod.relativePath
+    );
+
+    if (!specifier.declaration.node.source) {
+      return;
+    }
+    id = mod.getModule(specifier.declaration.node.source.value).id;
+    prelude.push(b.expressionStatement(
+      b.callExpression(b.identifier('__es6_export__'), [
+        b.literal(specifier.name),
+        b.memberExpression(
+          b.identifier(id),
+          b.literal(specifier.from),
+          true
+        )
+      ])
+    ));
+  });
+
+  return prelude;
+};
+
+module.exports = AMDFormatter;
